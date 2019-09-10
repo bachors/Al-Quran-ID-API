c2d53bee64047056c6eff930ee2f259182f78b15
Ican Bachors <bachor.can@gmail.com>
2019-09-10 14:02:59 +0700

    Create tes.js

---
+++ tes.js
@@ -0,0 +1,26 @@
+var data = [ 
+	{
+	  "arti" : "Pembukaan",
+	  "asma" : "الفاتحة",
+	  "audio" : "http://ia802609.us.archive.org/13/items/quraninindonesia/001AlFaatihah.mp3",
+	  "ayat" : 7,
+	  "keterangan" : "Surat <i>Al Faatihah</i> (Pembukaan) yang diturunkan di Mekah dan terdiri dari 7 ayat adalah surat yang ...",
+	  "nama" : "Al Fatihah",
+	  "nomor" : "1",
+	  "rukuk" : "1",
+	  "type" : "mekah",
+	  "urut" : "5"
+	}, 
+	{
+	  "arti" : "Sapi Betina",
+	  "asma" : "البقرة",
+	  "audio" : "http://ia802609.us.archive.org/13/items/quraninindonesia/002AlBaqarah.mp3",
+	  "ayat" : 286,
+	  "keterangan" : "Surat <i>Al Baqarah</i> yang 286 ayat itu turun di Madinah yang sebahagian besar diturunkan pada permulaan ...",
+	  "nama" : "Al Baqarah",
+	  "nomor" : "2",
+	  "rukuk" : "40",
+	  "type" : "madinah",
+	  "urut" : "87"
+	}
+];
